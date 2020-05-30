80e11f3635fe4604628f1f7158aabfc14e441e21
noituri <yknomeh@protonmail.com>
2020-05-30 13:01:28 +0200

    Added support for other versions of discord.js

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
-const fetch = require("node-fetch")
-const { UPDATE_BOT } = require("./endpoints.js")
+const fetch = require("node-fetch");
+const { UPDATE_BOT } = require("./endpoints.js");
 
 const createRequest = (token, body) => ({
     method: "POST",
@@ -8,22 +8,32 @@ const createRequest = (token, body) => ({
         "content-type": "application/json",
     },
     body: JSON.stringify(body)
-})
+});
+
+const clientServers = client => {
+    if (client.guilds.cache !== undefined)
+        return client.guilds.cache.size;
+    if (client.guilds.size !== undefined)
+        return client.guilds.size;
+
+    console.warn("Could not retrieve amount of servers from the client");
+    return 0;
+};
 
 exports.connectBdlBot = async (token, client) => {
-    console.info("Attempting to update bdl bot in 5 min")
+    console.info("Attempting to update bdl bot in 5 min");
 
     while (true) {
-        await new Promise(resolve => setTimeout(resolve, 300000))
-        fetch(UPDATE_BOT, createRequest(token, {"servers": client.guilds.cache.size}))
+        await new Promise(resolve => setTimeout(resolve, 300000));
+        fetch(UPDATE_BOT, createRequest(token, {"servers": clientServers(client)}))
             .then(resp => resp.json())
             .then(json => {
                 if (json.errors !== undefined) {
-                    console.error("Could not update the bdl bot. Make sure that your token is correct")
+                    console.error("Could not update the bdl bot. Make sure that your token is correct");
                 } else if (json.status !== "ok") {
-                    console.error("Could not update the bdl bot. Error: " + json.message)
+                    console.error("Could not update the bdl bot. Error: " + json.message);
                 }
             })
-            .catch(error => console.error(error))
+            .catch(error => console.error(error));
     }
 }
--- src/test.js
+++ src/test.js
@@ -1,3 +1,4 @@
-const { connectBdlBot } = require("./lib.js")
+const { connectBdlBot } = require("./lib.js");
 
-connectBdlBot("c6f76149-9e9d-4116-8e0a-dd049b6afb60", { guilds: { cache: { size: 10 } } })
+connectBdlBot("c6f76149-9e9d-4116-8e0a-dd049b6afb60", { guilds: { cache: { size: 10 } } });
+connectBdlBot("c6f76149-9e9d-4116-8e0a-dd049b6afb60", { guilds: { size: 12 } });
